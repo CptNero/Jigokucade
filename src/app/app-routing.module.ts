bbac3561be6ce514ffd93e0e1309c277d780fcf4
adrian.ravai@optimaster.eu <ravaiadrian@gmail.com>
2020-04-19 18:03:26 +0200

    did things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GameCollectionComponent} from "./components/game-collection/game-collection.component";
+import {GameComponent} from "./components/game/game.component";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent},
+  { path: 'game-collection', component: GameCollectionComponent},
+  { path: 'game/:id', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameCollectionComponent } from './components/game-collection/game-collection.component';
+import {NgMetro4Module} from "ng-metro4";
+import { GameComponent } from './components/game/game.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { TetrisComponent } from './components/tetris/tetris.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameCollectionComponent,
+    GameComponent,
+    WelcomeComponent,
+    CommentsComponent,
+    TetrisComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMetro4Module
   ],
   providers: [],
   bootstrap: [AppComponent]
